5e524d49bbb150f9e8ffaf3592cfb679aee34a1d
Sakamoto Hiroya <ayorih16@icloud.com>
2022-12-29 10:24:49 +0900

    add perlin noise test

--- src/test_src/perlinNoiseTest.py
+++ src/test_src/perlinNoiseTest.py
@@ -1,4 +1,7 @@
-from ..src import perlinnoise as pn
+import sys
+sys.path.append('../')
+
+import perlinnoise as pn
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -6,7 +9,7 @@ import random
 # set seed value in 0 ~ 100000
 seed = random.randint(0, 100000)
 
-x = y = np.arange(-5, 5, 0.1)
+x = y = np.arange(-3, 3, 0.05)
 
 X, Y = np.meshgrid(x, y)
 
@@ -16,5 +19,6 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 ax.plot_surface(X, Y, z, cmap='terrain')
+ax.view_init(elev=90, azim=0)
 
 plt.show()
